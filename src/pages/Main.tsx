3fdbce2049eeef08f9b628bab001e906a1a0a18d
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-01-18 23:38:47 +0100

    💄 Loader, spin when calling gsheet api

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { MdOutlineSave } from "react-icons/md"
 import { Card } from '../components/card/Card';
+import { Loader } from '../components/loader/Loader';
 
 import { NameManager } from '../components/nameManager/NameManager';
 import { SchemaContext } from '../components/SchemaContext';
@@ -13,12 +14,14 @@ import "./Home.scss"
 
 
 function Main() {
+	const [loading, setLoading] = useState(false)
 	const [schemas, setSchemas] = useState<SchemasList>({})
 	var sheetApi = new SheetApi(localStorage.getItem('sheetId') || '')
 	const capitalize = (s: string) => s && s[0].toUpperCase() + s.slice(1)
 
 	async function updateSchema() {
 		if (localStorage.getItem('sheetId')) {
+			setLoading(true)
 			let sheetNumber = await sheetApi.getNumberOfSheets()
 			for (let i = sheetApi.disabled; i < sheetNumber; i++) {
 				let sheet = await sheetApi.getSchema(i)
@@ -30,6 +33,7 @@ function Main() {
 					}
 				}));
 			}
+			setLoading(false)
 		}
 	}
 
@@ -57,23 +61,24 @@ function Main() {
 			<SheetIdManager updateSheetId={updateSheetId} />
 			<NameManager />
 			<h2 className="h2_helper">Step 3 ‒ Select days :</h2>
-			{Object.keys(schemas).map((week, sheetIndex) => {
-				if (checkIfDisplayable(schemas[week].schema)) {
-					return <div key={sheetIndex}>
-						<h1 className='weeks_separator'>{capitalize(week)}</h1>
-						<div className="cards_container">
-							{
-								Object.keys(schemas[week].schema).map((day, index) => (
-									Object.keys(schemas[week].schema[day].options).length ?
-										<Card day={day} schemaDays={schemas[week].schema[day]} sheetIndex={sheetApi.correctIndex(sheetIndex)} index={index} sheetApi={sheetApi} sheetSplit={schemas[week].splits} key={index} />
-										: null
-								))
-							}
+			{
+				loading ? <div className='cards_loader'><Loader /></div> : Object.keys(schemas).map((week, sheetIndex) => {
+					if (checkIfDisplayable(schemas[week].schema)) {
+						return <div key={sheetIndex}>
+							<h1 className='weeks_separator'>{capitalize(week)}</h1>
+							<div className="cards_container">
+								{
+									Object.keys(schemas[week].schema).map((day, index) => (
+										Object.keys(schemas[week].schema[day].options).length ?
+											<Card day={day} schemaDays={schemas[week].schema[day]} sheetIndex={sheetApi.correctIndex(sheetIndex)} index={index} sheetApi={sheetApi} sheetSplit={schemas[week].splits} key={index} />
+											: null
+									))
+								}
+							</div>
 						</div>
-					</div>
-				}
-				return null
-			})
+					}
+					return null
+				})
 			}
 
 		</SchemaContext.Provider>
