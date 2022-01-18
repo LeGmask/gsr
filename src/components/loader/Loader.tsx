56933fd704d294a4070146b39c04b2d8fa211398
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-01-18 23:59:17 +0100

    💄 Modern spinner

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -4,15 +4,30 @@ import './Loader.scss'
 export function Loader () {
   return (
 	<div className="loader">
-		<div className="dot dot1"><i></i></div>
-		<div className="dot dot2"><i></i></div>
-		<div className="dot dot3"><i></i></div>
-		<div className="dot dot4"><i></i></div>
-		<div className="dot dot5"><i></i></div>
-		<div className="dot dot6"><i></i></div>
-		<div className="dot dot7"><i></i></div>
-		<div className="dot dot8"><i></i></div>
-		<div className="dot dot9"><i></i></div>
+		<div className="wave top-wave">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		</div>
+		<div className="wave bottom-wave">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		</div>
 	</div>
   );
 }
